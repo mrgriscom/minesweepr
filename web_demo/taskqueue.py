093b2c6f452cb9ad53c7fd4fa47068fea334ac62
Drew Roos <droos@dimagi.com>
2011-11-06 12:18:46 -0500

    track running time

--- web_demo/taskqueue.py
+++ web_demo/taskqueue.py
@@ -28,6 +28,22 @@ def worker_loop(inq, outq):
         logging.debug('worker %s completed task %d: %s' % (os.getpid(), job_id, str((success, result))))
         outq.put((job_id, success, result))
 
+class PendingTask(object):
+    def __init__(self, callback, time_limit):
+        self.callback = callback
+        self.time_limit = time_limit
+        self.received_at = time.time()
+
+    def expiry(self):
+        return self.received_at + self.time_limit if self.time_limit is not None else None
+
+    def is_expired(self, now=None):
+        expires = self.expiry()
+        if expires is None:
+            return False
+        else:
+            return (now if now is not None else time.time()) > expires
+
 class Pool(threading.Thread):
     def __init__(self, num_workers):
         threading.Thread.__init__(self)
@@ -36,7 +52,7 @@ class Pool(threading.Thread):
 
         self.num_workers = num_workers
         self.job_counter = 0
-        self.callbacks = {}
+        self.pending = {}
 
         self.outq = mp.Queue()
         self.inq = mp.Queue()
@@ -48,7 +64,7 @@ class Pool(threading.Thread):
         return w
 
     def apply_async(self, callback, func, args=[], kwargs={}, time_limit=None):
-        job_id = self.new_job(callback)
+        job_id = self.new_job(callback, time_limit)
         logging.debug('new task %d: %s' % (job_id, str((func, args, kwargs, time_limit))))
         self.outq.put((job_id, func, args, kwargs))
 
@@ -70,17 +86,21 @@ class Pool(threading.Thread):
         while True:
             job_id, success, result = self.inq.get()
             status = {True: 'success', False: 'exception'}[success]
-            with self.lock:
-                callback = self.callbacks[job_id]
-                del self.callbacks[job_id]
-            logging.debug('task %d complete: %s' % (job_id, str((status, result))))
-            callback(status, result)
 
-    def new_job(self, callback):
+            with self.lock:
+                task = self.pending[job_id]
+                del self.pending[job_id]
+
+            clock = time.time()
+
+            logging.debug('task %d complete: %s' % (job_id, str((status, result))))
+            task.callback(status, result, clock - task.received_at)
+
+    def new_job(self, callback, time_limit):
         with self.lock:
             job_id = self.job_counter
             self.job_counter += 1
-            self.callbacks[job_id] = callback
+            self.pending[job_id] = PendingTask(callback, time_limit)
             return job_id
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
@@ -107,12 +127,12 @@ class TaskRequestHandler(BaseHTTPRequestHandler):
                 self.send_error(*e.args)
                 return
 
-            status, result = self.server.pool.apply(func, args, kwargs, time_limit)
+            status, result, runtime = self.server.pool.apply(func, args, kwargs, time_limit)
 
             self.send_response(200)
             self.send_header('Content-Type', 'text/json')
             self.end_headers()
-            self.wfile.write(json.dumps({'status': status, 'result': result}))
+            self.wfile.write(json.dumps({'status': status, 'result': result, 'runtime': runtime}))
         except Exception, e:
             logging.exception('unexpected error')
             self.send_error(500, '%s %s' % (type(e), str(e)))
@@ -157,7 +177,8 @@ if __name__ == "__main__":
     logging.info('process pool started with %d workers' % opts.num_workers)
 
     # also proof-of-concept'ed with tornado, but requests were getting dropped
-    # under high load
+    # under high load. python http server spawns a thread for each request, but
+    # i think that's acceptable
     gw = TaskQueueHTTPGateway(opts.port, pool)
     gw.start()
     logging.info('gateway started on port %d' % opts.port)
