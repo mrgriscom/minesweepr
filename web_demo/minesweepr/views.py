705495d10265e1473fe170164ef76875f21bd5d8
Drew Roos <droos@dimagi.com>
2011-11-27 16:55:42 -0500

    more verbose logging

--- web_demo/minesweepr/views.py
+++ web_demo/minesweepr/views.py
@@ -8,6 +8,7 @@ import logging
 import time
 from tasks import minesweeper_solve
 from taskexec import exec_capped, ExecTimeOut
+import itertools
 
 @csrf_exempt
 def api_solve(request):
@@ -19,10 +20,23 @@ def api_solve(request):
         result = exec_capped(minesweeper_solve, settings.CPU_QUOTA, payload)
     except ExecTimeOut:
         result = {'error': 'cpu quota exceeded'}
-    logging.info('task queue rtt %.3f' % (time.time() - start))
+    log_result(payload, result, time.time() - start)
 
     logging.debug('<<' + str(result))
     return HttpResponse(json.dumps(result), 'text/json')
+
+def log_result(payload, result, rtt):
+    num_rules = len(payload['rules'])
+    num_uniq_cells = len(set(itertools.chain(*(r['cells'] for r in payload['rules']))))
+    avg_cells_per_rule = sum(len(r['cells']) for r in payload['rules']) / float(num_rules) if num_rules else 0.
+
+    try:
+        solved_in = '%.3f' % result['processing_time']
+    except KeyError:
+        solved_in = '--'
+
+    logging.info('%d rules %d cells %.1f avg cpr; solved in %s, task queue rtt %.3f' %
+                 (num_rules, num_uniq_cells, avg_cells_per_rule, solved_in, rtt))
 
 def template_static(request):
     url = request.path[1:-1]
